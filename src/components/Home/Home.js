012cf2573ba560d68dae86b149916bf85a1b2f88
Clayton Pabst <claytonpabst@gmail.com>
2017-12-06 03:20:48 -0700

    started.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 import './Home.css';
+import data from './data.js';
 
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = data
+    
+    this.loadTwoPictures = this.loadTwoPictures.bind(this);
+  }
+  componentDidMount(){
+    this.loadTwoPictures()
+  }
+
+  loadTwoPictures(){
+    let pictureOne = {};
+    let pictureTwo = {};
+    for(let i=0; i<this.state.pictures.length; i++){
+      
+    }
+  }
 
   render() {
+    console.log(this.state);
     return (
       <div className="home">
-
-          Home Page
+        <div className='pictureToVoteWrapper'>
+          <img className='pictureToVotePicture' src={this.state.pictures[0].URL} alt=""/>
+          <button className='pictureToVoteButton'>WINNER</button>
+        </div>
+        <div className='pictureToVoteWrapper'>
+          <img className='pictureToVotePicture' src={this.state.pictures[1].URL} alt=""/>
+          <button className='pictureToVoteButton'>WINNER</button>
+        </div>
 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import {unregister} from './registerServiceWorker';
 
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
-import store from './store';
+// import store from './store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
+    {/*<Provider store={ store }>*/}
       <App />
-    </Provider>
+    {/*</Provider>*/}
   </BrowserRouter>,
   document.getElementById('root')
 );
