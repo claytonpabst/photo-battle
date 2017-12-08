70301d16a89c184a7a4cf921970bd7ea806a34d4
Clayton Pabst <claytonpabst@gmail.com>
2017-12-08 03:30:52 -0700

    Sorting through the pictures is working great.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,28 +11,32 @@ class Home extends Component {
     this.state = data
     
     this.loadTwoPictures = this.loadTwoPictures.bind(this);
-    this.loadTwoBetaPictures = this.loadTwoBetaPictures.bind(this);
+    this.updatePicturesInRound_and_getTwoNewPictures = this.updatePicturesInRound_and_getTwoNewPictures.bind(this);
   }
   componentDidMount(){
-    this.loadTwoPictures()
+    this.decideOnBetaOrStandard()
   }
 
-  loadTwoPictures(){
-    let potentialPictures = [];
-    let pictureOne = {};
-    let pictureTwo = {};
+  decideOnBetaOrStandard(){
+    let potentialBetaPictures = [];
+    let potentialStandardPictures = [];
     for(let i=0; i<this.state.pictures.length; i++){
       if(this.state.pictures[i].roundsVoted < 10){
-        potentialPictures.push(this.state.pictures[i])
+        potentialBetaPictures.push(this.state.pictures[i])
+      } else {
+        potentialStandardPictures.push(this.state.pictures[i])
       }
     }
-    if(potentialPictures.length >= 2){
-      this.loadTwoBetaPictures(potentialPictures)
+    if(this.state.roundOfVoting <= 10){
+      this.loadTwoPictures(potentialBetaPictures);
+      return
+    } else {
+      this.loadTwoPictures(potentialStandardPictures);
       return
     }
   }
 
-  loadTwoBetaPictures(potentialPictures){
+  loadTwoPictures(potentialPictures){
     let i1 = this.randomNumber(0, potentialPictures.length-1);
     let i2 = this.randomNumber(0, potentialPictures.length-1);
     while(i2 === i1){
@@ -47,17 +51,37 @@ class Home extends Component {
   updatePicturesInRound_and_getTwoNewPictures(winningPicture, losingPicture){
     let winner;
     let loser;
+    let winnerIndex;
+    let loserIndex;
+    let pictures = this.state.pictures;
+    let roundOfVoting = this.state.roundOfVoting;
     for(let i=0; i<this.state.pictures.length; i++){
-      if(winningPicture.id === this.state.pictures[i].id){
-        winner = this.state.pictures[i]
+      if(winningPicture.id === pictures[i].id){
+        winner = pictures[i];
+        winnerIndex = i;
+      }
+      if(losingPicture.id === pictures[i].id){
+        loser = pictures[i]
+        loserIndex = i;
+      }
+      if(winner && loser){
+        break;
       }
     }
-    for(let i=0; i<this.state.pictures.length; i++){
-      if(losingPicture.id === this.state.pictures[i].id){
-        loser = this.state.pictures[i]
-      }
-    }
-    
+    console.log(winner, loser);
+    winner.wins ++;
+    winner.roundsVoted ++;
+    loser.losses ++;
+    loser.roundsVoted ++;
+    pictures[winnerIndex] = winner;
+    pictures[loserIndex] = loser;
+    roundOfVoting++;
+    this.setState({
+      pictures: pictures,
+      roundOfVoting: roundOfVoting
+    }, () => {
+      this.decideOnBetaOrStandard();
+    });
   }
 
   randomNumber(min, max){
@@ -70,13 +94,20 @@ class Home extends Component {
     console.log(this.state.pictureTwo)
     return (
       <div className="home">
+        <h1>Round of Voting: {this.state.roundOfVoting}</h1>
         <div className='pictureToVoteWrapper'>
           <img className='pictureToVotePicture' src={this.state.pictureOne.URL} alt=""/>
-          <button onClick={() => (this.updatePicturesInRound_and_getTwoNewPictures(this.state.pictureOne.id, this.state.pictureTwo.id))} className='pictureToVoteButton'>WINNER</button>
+          <button onClick={() => (this.updatePicturesInRound_and_getTwoNewPictures(this.state.pictureOne, this.state.pictureTwo))} className='pictureToVoteButton'>WINNER</button>
+          <h1>wins:{this.state.pictureOne.wins}</h1>
+          <h1>losses:{this.state.pictureOne.losses}</h1>
+          <h1>roundsVoted:{this.state.pictureOne.roundsVoted}</h1>
         </div>
         <div className='pictureToVoteWrapper'>
           <img className='pictureToVotePicture' src={this.state.pictureTwo.URL} alt=""/>
-          <button onClick={() => (this.updatePicturesInRound_and_getTwoNewPictures(this.state.pictureTwo.id, this.state.pictureOne.id))} className='pictureToVoteButton'>WINNER</button>
+          <button onClick={() => (this.updatePicturesInRound_and_getTwoNewPictures(this.state.pictureTwo, this.state.pictureOne))} className='pictureToVoteButton'>WINNER</button>
+          <h1>wins:{this.state.pictureTwo.wins}</h1>
+          <h1>losses:{this.state.pictureTwo.losses}</h1>
+          <h1>roundsVoted:{this.state.pictureTwo.roundsVoted}</h1>
         </div>
 
       </div>
